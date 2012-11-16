c6013cf5ad8b9ee33c0dd5b03477bf6b121fc174
Katre Paabo <katrepaabo@hot.ee>
2012-11-16 19:48:11 +0200

    Lisatud wcf toorik

---
+++ TurakasServiceLibrary/ITeenus.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace TurakasServiceLibrary
+{
+
+    [ServiceContract(CallbackContract = typeof(ITeenusCallback))]
+    public interface ITeenus
+    {
+        [OperationContract]
+        ServicePlayer[] getSubscribers(ServiceGame game);
+
+        [OperationContract]
+        int getNewGameId(ServicePlayer p);
+
+        [OperationContract]
+        void addPlayerToGame(ServiceGame game);
+
+        [OperationContract]
+        ServiceCard hit(ServiceCard target, ServiceCard hit);
+
+        [OperationContract]
+        ServiceCard move(ServiceCard card);
+
+        [OperationContract]
+        ServiceCard subscribeTogame(int game, string name);
+    }
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class ServiceGame
+    {
+        private int _ID;
+        private ServicePlayer[] _players;
+        private int _count;
+
+        [DataMember]
+        public int Count
+        {
+            get { return _count; }
+            set { _count = value; }
+        }
+
+        [DataMember]
+        public ServicePlayer[] Players
+        {
+            get { return _players; }
+            set { _players = value; }
+        }
+
+        [DataMember]
+        public int ID
+        {
+            get { return _ID; }
+            set { _ID = value; }
+        }
+        
+    }
+
+    [DataContract]
+    public class ServicePlayer 
+    {
+        private string _nickName;
+        private int _id;
+        [DataMember]
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        [DataMember]
+        public string Name
+        {
+            get { return _nickName; }
+            set { _nickName = value; }
+        }
+    }
+    [DataContract]
+    public class ServiceCard{
+        
+        private int _kind;
+        private int _rank;
+        [DataMember]
+        public int Rank
+        {
+         get { return _rank; }
+         set { _rank = value; }
+        }
+        [DataMember]
+        public int Kind
+        {
+          get { return _kind; }
+          set { _kind = value; }
+        }
+    }
+
+    public interface ITeenusCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void onChatMessage(string from, string message);
+
+        [OperationContract(IsOneWay = true)]
+        void onGetContacts(string[] contacts);
+
+        [OperationContract(IsOneWay = true)]
+        void onHit(ServiceCard hit);
+
+        [OperationContract(IsOneWay = true)]
+        void onMove(ServiceCard card);
+    }
+}
---
+++ TurakasServiceLibrary/Teenus.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace TurakasServiceLibrary
+{
+    
+    public class Teenus : ITeenus
+    {
+        /** 
+         * List of all subscribers to the chat service. Entries to this list are 
+         * added by Subscribe() method and removed by Unsubscribe() method.
+         */
+        private static readonly List<Subscriber> subscrbers = new List<Subscriber>();
+        /**
+         *List to hold active Games 
+         */
+        private static readonly List<ServiceGame> games = new List<ServiceGame>();
+
+        /** Get the subscriber entry for method caller */
+        public Subscriber GetMe()
+        {
+            ITeenusCallback cb = OperationContext.
+                                        Current.
+                                            GetCallbackChannel<ITeenusCallback>();
+            //return subscrbers.Single(item => item.callback == cb);
+            foreach (Subscriber s in subscrbers)
+            {
+                if (s.callback.Equals(cb))
+                    return s;
+            }
+            return null;
+        }
+
+        public ServicePlayer[] getSubscribers(ServiceGame game)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int getNewGameId(ServicePlayer p)
+        {
+            ServiceGame g = new ServiceGame();
+            g.Players = new ServicePlayer[6];
+            g.Players[0] = p;
+            g.Count = 1;
+            g.ID = games.Count + 1;
+            games.Add(g);
+            return g.ID;
+        }
+
+        public void addPlayerToGame(ServiceGame game)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ServiceCard hit(ServiceCard target, ServiceCard hit)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public ServiceCard move(ServiceCard card)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+    public class Subscriber
+    {
+        /* Nickname for subscriber */
+        public string nickname;
+        /* Interface for message notify */
+        public ITeenusCallback callback;
+
+        public Subscriber(string nickname)
+        {
+            this.nickname = nickname;
+            this.callback = OperationContext.
+                                Current.
+                                    GetCallbackChannel<ITeenusCallback>();
+        }
+
+        public bool IsChannelOpen()
+        {
+            return ((ICommunicationObject)callback).State == CommunicationState.Opened;
+        }
+    }
+}
